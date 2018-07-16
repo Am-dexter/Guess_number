be2dfc2a6c46633688871133f2333e12e10c2da1
Am-dexter <koskemichael@gmail.com>
2018-07-16 21:27:15 +0300

    Guess number code

--- guessnumber.py
+++ guessnumber.py
@@ -7,17 +7,18 @@ print()
 
 the_number = random.randint(0, 100)
 guess = -1
+name = input('What is your name?')
 
 while guess != the_number:
     guess_text = input('Guess a number between 0 and 100: ')
     guess = int(guess_text)
 
     if guess < the_number:
-        print('Too low')
+        print('Sorry {}, your number {} was TOO LOW' .format(name, guess))
     elif guess > the_number:
-        print('Too high')
+        print('Sorry {}, your number {} was TOO HIGH' .format(name, guess))
     else:
-        print('You are a Winner!!!')
+        print('Congratulation {}, you Won!! Your number was {}' .format(name, guess))
 
 print('Done!')
 
